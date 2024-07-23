ff40a05c05576eea6b5bbdd1fcbe251a0309a50b
Marcélio G. Pereira <marcelio.pereira@tse.jus.br>
2024-07-23 01:56:43 -0300

    Curso 2 - Aula 4
    Conclusão da Aula 4 do curso 2 de android.

--- app/src/main/java/com/example/alura_agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/com/example/alura_agenda/ui/activity/ListaAlunosActivity.java
@@ -43,7 +43,8 @@ public class ListaAlunosActivity extends AppCompatActivity {
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
-        menu.add("Remover");
+        getMenuInflater().inflate(R.menu.activity_lista_alunos_menu, menu);
+        //menu.add("Remover");
     }
 
     private void configuraFabNovoAluno() {
@@ -58,10 +59,13 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
     public boolean onContextItemSelected(@NonNull MenuItem item) {
-        AdapterView.AdapterContextMenuInfo menuInfo =
-                (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
-        Aluno alunoEscolhido = adapter.getItem(menuInfo.position);
-        remove(alunoEscolhido);
+        int itemId = item.getItemId();
+        if (itemId == R.id.activity_lista_alunos_menu_remover) {
+            AdapterView.AdapterContextMenuInfo menuInfo =
+                    (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+            Aluno alunoEscolhido = adapter.getItem(menuInfo.position);
+            remove(alunoEscolhido);
+        }
         return super.onContextItemSelected(item);
     }
 
